02c6372cb0f45015c5653d0a3862905a413ae119
tedtke <tedtke514@gmail.com>
2021-03-17 14:42:44 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+class EyesOnMe extends React.Component {
+  handleKeyUp = (event) => {
+    console.log("Entering Password...");
+  };
+
+  render() {
+    return <div input type="password" onKeyUp={this.handleKeyUp}></div>;
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+import React, { Component } from "react";
+
+class Keypad extends React.Component {
+  render() {
+    // return ()
+  }
+}
+
+export default Keypad;
